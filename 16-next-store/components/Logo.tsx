897d5010d04d22cf83d15d8aa54989cc4136acc9
Alberto Raúl José <albertorauljose2@gmail.com>
2025-04-19 16:20:16 +0000

    feat: install lucid-react package and create Logo component

---
+++ 16-next-store/components/Logo.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+import { Code } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+
+const Logo = () => {
+	return (
+		<Button
+			size='icon'
+			asChild
+		>
+			<Link href='/'>
+				<Code aria-hidden='true' className='size-6' />
+				<span className='sr-only'>Next Store - Home</span>
+			</Link>
+		</Button>
+	);
+};
+
+export default Logo;
---
+++ 16-next-store/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import Container from '@/components/global/Container';
+
+const Navbar = () => {
+	return (
+		<nav className='border-b'>
+			<Container>{''}</Container>
+		</nav>
+	);
+};
+
+export default Navbar;
