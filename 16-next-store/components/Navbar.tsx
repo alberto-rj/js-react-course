c55ff79cdac924fc2ed5a248b1d2c99556405d70
Alberto Raúl José <albertorauljose2@gmail.com>
2025-04-19 18:24:35 +0000

    feat(navigation-menu): Create Navbar component

--- 16-next-store/components/Navbar.tsx
+++ 16-next-store/components/Navbar.tsx
@@ -1,11 +1,23 @@
+import CartButton from '@/components/CartButton';
 import Container from '@/components/Container';
+import LinksDropdown from '@/components/LinksDropdown';
+import Logo from '@/components/Logo';
+import NavSearch from '@/components/NavSearch';
+import ThemePicker from '@/components/ThemePicker';
 
-const Navbar = () => {
+function Navbar() {
 	return (
 		<nav className='border-b'>
-			<Container>{''}</Container>
+			<Container className='flex flex-col sm:flex-row  sm:justify-between sm:items-center sm:flex-wrap gap-4 py-8'>
+				<Logo />
+				<NavSearch />
+				<div className='flex gap-4 items-center '>
+					<CartButton />
+					<ThemePicker />
+					<LinksDropdown />
+				</div>
+			</Container>
 		</nav>
 	);
-};
-
+}
 export default Navbar;
