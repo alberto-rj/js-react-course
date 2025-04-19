9d2584506ec651e4713aa060f1f3ce625b8b79cc
Alberto Raúl José <albertorauljose2@gmail.com>
2025-04-19 10:45:38 +0000

    complete lesson: local image

--- 15-nextjs-turorial/app/tours/[slug]/page.tsx
+++ 15-nextjs-turorial/app/tours/[slug]/page.tsx
@@ -1,5 +1,9 @@
 import Link from 'next/link';
 
+import Image from 'next/image';
+
+import mapsImage from '@/images/maps.jpg';
+
 type Props = {
 	params: {
 		slug: string;
@@ -11,6 +15,20 @@ const page = ({ params: { slug } }: Props) => {
 		<div>
 			<h1 className='text-6xl'>Page placeholder</h1>
 			<p>Slug: {slug}</p>
+			<section className='flex gap-4 mt-4'>
+				{/* local image */}
+				<div className='max-w-52 max-h-52 overflow-hidden rounded'>
+					<Image
+						src={mapsImage}
+						alt='Maps'
+						width={208}
+						height={208}
+						className='object-cover size-full'
+					/>
+				</div>
+				{/* remote image */}
+				<div></div>
+			</section>
 			<Link
 				href='/tours'
 				className='text-blue-700 underline hover:no-underline hover:text-blue-900'
