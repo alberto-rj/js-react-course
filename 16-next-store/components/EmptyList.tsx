b5039a74bb077882f91227160f61f5aa7d249932
Alberto Raúl José <albertorauljose2@gmail.com>
2025-04-20 11:48:44 +0000

    feat: create EmptyList component

---
+++ 16-next-store/components/EmptyList.tsx
@@ -0,0 +1,12 @@
+import { cn } from '@/lib/utils';
+
+type Props = {
+	heading?: string;
+	className?: string;
+};
+
+const EmptyList = ({ heading = 'No items found.', className }: Props) => (
+	<h2 className={cn('text-xl', className)}>{heading}</h2>
+);
+
+export default EmptyList;
