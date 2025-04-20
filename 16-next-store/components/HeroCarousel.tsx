51767189acad39b81b5273f951019ca8bea2c565
Alberto Raúl José <albertorauljose2@gmail.com>
2025-04-20 12:40:12 +0000

    feat: Create HeroCarousel Component

---
+++ 16-next-store/components/HeroCarousel.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from '@/components/ui/carousel';
+import { Card, CardContent } from '@/components/ui/card';
+
+const carouselImages = [
+	'/images/hero1.jpg',
+	'/images/hero2.jpg',
+	'/images/hero3.jpg',
+	'/images/hero4.jpg',
+];
+
+const HeroCarousel = () => {
+	return (
+		<div className='hidden lg:block'>
+			<Carousel>
+				<CarouselContent>
+					{carouselImages.map((image, index) => {
+						return (
+							<CarouselItem key={index}>
+								<Card>
+									<CardContent className='p-2'>
+										<Image
+											src={image}
+											alt='hero'
+											className='w-full h-[24rem] rounded-md object-cover'
+										/>
+									</CardContent>
+								</Card>
+							</CarouselItem>
+						);
+					})}
+				</CarouselContent>
+				<CarouselPrevious />
+				<CarouselNext />
+			</Carousel>
+		</div>
+	);
+};
+
+export default HeroCarousel;
